9a7a36dc735ee840d0226790db323fe18b4b5b1f
VBBoyanov22 <VBBoyanov22@codingburgas.bg>
2024-03-27 22:36:36 +0200

    Add premake

---
+++ DigitalSchool/src/DigitalSchool.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Function declaration
+void OpenURL(const char* url);
+
+int main() {
+    const char* url = "https://codingburgas.org";
+    OpenURL(url);
+    return 0;
+}
+
+// Function definition
+void OpenURL(const char* url) {
+    // Implementation here
+}
