964aa0cd20727a02193d5e8db2a5d93f0c55e9b2
VBBoyanov22 <VBBoyanov22@codingburgas.bg>
2024-04-11 22:41:57 +0300

    Fix file structure

--- DigitalSchool/Digital School/Digital School/precompile.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include "../../../vendor/raylib/include/raylib.h"
--- DigitalSchool/Digital School/DigitalSchool.cpp
+++
@@ -1,5 +0,0 @@
-#include "includes/precompile.h"
-
-int main() {
-	InitWindow(1920, 1080, "Digital Testing");
-}
