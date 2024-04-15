02ed4a92741cd65a955316ae99a4d77ce81a5f60
MDBelchev21 <MDBelchev21@codingburgas.bg>
2024-04-15 21:05:08 +0300

    Add login function. Add file for register function.

---
+++ BLL/src/login.cpp
@@ -0,0 +1,26 @@
+#include "../includes/precompile.h"
+
+bool login(std::fstream& loginFile)
+{
+    bool check = false;
+    std::string loginUsername;
+    std::string loginPassword;
+    std::string line;
+    std::string inputLine;
+
+    std::cin >> loginUsername;
+    std::cin >> loginPassword;
+
+    inputLine = loginUsername + " " + loginPassword;
+
+    while (!loginFile.eof())
+    {
+        getline(loginFile, line);
+        if (line == inputLine)
+        {
+            check = true;
+            break;
+        }
+    }
+    return check;
+}
