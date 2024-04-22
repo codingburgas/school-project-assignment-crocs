519e24b2dcdadf448a2c912f596c70c4bf9a5c3c
VBBoyanov22 <VBBoyanov22@codingburgas.bg>
2024-04-22 23:38:09 +0300

    Make the premake create BLL and DAL projects and link them to DigitalSchool project

--- premake5.lua
+++ premake5.lua
@@ -6,34 +6,83 @@ workspace "DigitalSchool"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-project "DigitalSchool"
-
-    kind "ConsoleApp"
-    
+-- Create a static library project for Business Logic Layer (BLL)
+project "BLL"
+    location "BLL/BLL"
+    kind "StaticLib"
     language "C++"
     cppdialect "C++20"
 
-    targetdir ("./bin/".. outputdir.. "/%{prj.name}")
-    objdir ("./bin-int/".. outputdir.. "/%{prj.name}") 
+    targetdir ("bin/".. outputdir.. "/%{prj.name}")
+    objdir ("bin-int/".. outputdir.. "/%{prj.name}")
 
-    files { "./%{prj.name}/src/**.cpp", "./%{prj.name}/src/**.h" }
+    files { "BLL/src/**.cpp", "BLL/include/**.h" }
 
-    includedirs { "./vendor/raylib/include", "./%{prj.name}/src" }
-
-    libdirs { "./vendor/raylib/lib" }
-
-    links { "raylibdll" }
-
-    postbuildcommands { ("{COPY} ./vendor/raylib/lib/raylib.dll ./bin/".. outputdir.. "/%{prj.name}") }
+    includedirs { "BLL/include" }
 
     staticruntime "On"
-
     systemversion "latest"
 
     filter "configurations:Debug"
-      defines "OD_DEBUG"
-      symbols "On"
+        defines "OD_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "OD_RELEASE"
+        optimize "On"
+
+-- Create a static library project for Data Access Layer (DAL)
+project "DAL"
+    location "DAL/DAL"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+
+    targetdir ("bin/".. outputdir.. "/%{prj.name}")
+    objdir ("bin-int/".. outputdir.. "/%{prj.name}")
+
+    files { "DAL/src/**.cpp", "DAL/include/**.h" }
+
+    includedirs { "DAL/include" }
+
+    staticruntime "On"
+    systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "OD_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "OD_RELEASE"
+        optimize "On"
+
+-- Main application project
+project "DigitalSchool"
+    location "DigitalSchool"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+
+    targetdir ("bin/".. outputdir.. "/%{prj.name}")
+    objdir ("bin-int/".. outputdir.. "/%{prj.name}")
+
+    files { "DigitalSchool/src/**.cpp", "DigitalSchool/src/**.h" }
+
+    includedirs { "vendor/raylib/include", "DigitalSchool/src", "BLL/include", "DAL/include" }
+
+    libdirs { "vendor/raylib/lib" }
+
+    links { "raylibdll", "BLL", "DAL" }
+
+    postbuildcommands { ("{COPY} vendor/raylib/lib/raylib.dll bin/".. outputdir.. "/%{prj.name}") }
+
+    staticruntime "On"
+    systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "OD_DEBUG"
+        symbols "On"
     
     filter "configurations:Release"
-      defines "OD_RELEASE"
-      optimize "On"
+        defines "OD_RELEASE"
+        optimize "On"
